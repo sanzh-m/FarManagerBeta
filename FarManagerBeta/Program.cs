b2ba1d540529d997aa7a18bb96c9e1f15c2e68bf
sanzh-m <sanzh98@gmail.com>
2017-02-09 11:35:57 +0600

    almost final version

--- FarManagerBeta/Program.cs
+++ FarManagerBeta/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,24 @@ namespace FarManagerBeta
 {
     class Program
     {
+        static string delete (string s)
+        {
+            int i = s.Length-1;
+            if (s.Length < 4) return s;
+            while (!'\\'.Equals(s[i]))
+            {
+                --i;
+            }
+            string k = null;
+            for (int j=0; j<i; ++j)
+            {
+                k += s[j];
+            }
+            return k;
+        }
         static void print(List<Object> entries, int pos)
         {
+            Console.Clear();
             Console.SetCursorPosition(0, 0);
             int lineNumber = 0;
             foreach (Object entry in entries)
@@ -38,7 +55,8 @@ namespace FarManagerBeta
         }
         static void Main(string[] args)
         {
-            DirectoryInfo sanzhar = new DirectoryInfo(@"C:\Users\sanzh\Desktop");
+            string path = @"D:\Backup";
+            DirectoryInfo sanzhar = new DirectoryInfo(path);
             DirectoryInfo[] allSanzhars = sanzhar.GetDirectories();
             FileInfo[] allNeSanzhars = sanzhar.GetFiles();
             List<Object> entries = new List<object>();
@@ -76,6 +94,55 @@ namespace FarManagerBeta
                     {
                         pos++;
                     }
+                }
+                else if (sanzharEntered.Key == ConsoleKey.Enter)
+                {
+                    Object s = new Object();
+                    s = entries[pos];
+                    if (s.GetType() == typeof(DirectoryInfo))
+                    {
+                        path += @"\" + ((DirectoryInfo)entries[pos]).Name;
+                        sanzhar = new DirectoryInfo(path);
+                        allSanzhars = sanzhar.GetDirectories();
+                        allNeSanzhars = sanzhar.GetFiles();
+                        entries.Clear();
+                        entries = new List<object>();
+                        foreach (DirectoryInfo papka in allSanzhars)
+                        {
+                            entries.Add(papka);
+                        }
+                        foreach (FileInfo fail in allNeSanzhars)
+                        {
+                            entries.Add(fail);
+                        }
+                        pos = 0;
+
+                    }
+                    else
+                    {
+                        string k = path + @"\" + ((FileInfo)entries[pos]).Name;
+                        Process p = new Process();
+                        p.StartInfo.FileName = k;
+                        p.StartInfo.UseShellExecute = true;
+                        p.Start();
+                    }
+                }
+                if (sanzharEntered.Key == ConsoleKey.Backspace)
+                {
+                    path = delete(path);
+                    sanzhar = new DirectoryInfo(path);
+                    allSanzhars = sanzhar.GetDirectories();
+                    allNeSanzhars = sanzhar.GetFiles();
+                    entries = new List<object>();
+                    foreach (DirectoryInfo papka in allSanzhars)
+                    {
+                        entries.Add(papka);
+                    }
+                    foreach (FileInfo fail in allNeSanzhars)
+                    {
+                        entries.Add(fail);
+                    }
+                    pos = 0;
                 }
                 print(entries, pos);
             }
